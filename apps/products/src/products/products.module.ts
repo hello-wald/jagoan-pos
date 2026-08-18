79e01f87825815150376ed5fc2ebd1fb90fd141d
Catherine Alyssa <catherinealyssar@gmail.com>
2026-08-18 09:07:28 +0700

    fix: remove unused products module

--- apps/products/src/products/products.module.ts
+++ apps/products/src/products/products.module.ts
@@ -1,9 +1,9 @@
-import { Module } from "@nestjs/common";
-import { ProductsPrismaModule } from "../prisma/prisma.module";
-import { ProductStorageModule } from "../storage/product-storage.module";
-import { RedisModule } from "@jagoan-pos/redis";
-import { ProductsController } from "./products.controller";
-import { ProductsService } from "./products.service";
+import { Module } from '@nestjs/common';
+import { ProductsPrismaModule } from '../prisma/prisma.module';
+import { ProductStorageModule } from '../storage/product-storage.module';
+import { RedisModule } from '@jagoan-pos/redis';
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 
 @Module({
   imports: [ProductsPrismaModule, ProductStorageModule, RedisModule],
