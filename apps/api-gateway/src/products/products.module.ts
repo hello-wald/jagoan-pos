83671887822cbaa95279cab700c158e6be355a9f
Endra Anugrah <anugrahendra1@gmail.com>
2026-08-20 00:25:55 +0700

    ci: add GCP deployment pipeline

--- apps/api-gateway/src/products/products.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RpcClientsModule } from '../clients/clients.module';
-import { ProductsController } from './products.controller';
-
-@Module({
-  imports: [RpcClientsModule],
-  controllers: [ProductsController],
-})
-export class ProductsModule {}
